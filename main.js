62609d1d4bc3c4074e3b3e07b3974764c3a7a457
Joao Oliveira <88111035+jrOliv@users.noreply.github.com>
2022-07-31 01:10:24 -0300

    main.js 1.1.2
    Adicionado histórico de palpites e feature de foco no input.

--- main.js
+++ main.js
@@ -3,36 +3,52 @@ let num = Math.floor((Math.random()*100)+1);
 const hintField = document.querySelector('.hintField');
 const hintButton = document.querySelector('.hintButton');
 const hints = document.querySelector('.hints'); 
-const gameStatus = document.querySelectorAll('.gameStatus');
+const gameStatus = document.querySelectorAll('.gameStatus > p');
 console.log(num);
 
 hintButton.addEventListener('click', checkNumber);
 let cont = 0;
-
+hintField.focus();
 
 function checkNumber(){
     let userInput = Number(hintField.value);
     console.log(userInput);
     
     if(userInput === num){
-        gameStatus[0] = '-ACERTOU-';
-        gameStatus[1] = '';
+        gameStatus[0].textContent = '-ACERTOU-';
+        gameStatus[1].textContent = '';
+
+        //gameOver();
 
     } else if(cont === 6){
-        gameStatus[0] = '-FIM DE JOGO-';
-        gameStatus[1] = '';
+        gameStatus[0].textContent = '-FIM DE JOGO-';
+        gameStatus[1].textContent = '';
+
+        //gameOver();
 
     } else {
-        gameStatus[0] = '-ERRADO-';
+        gameStatus[0].textContent = '-ERRADO-';
 
         if(userInput > num){
-            gameStatus[1] = 'TENTE UM NUMERO MENOR';
+            gameStatus[1].textContent = 'TENTE UM NUMERO MENOR';
 
         } else {
-            gameStatus[1] = 'TENTE UM NUMERO MAIOR';
+            gameStatus[1].textContent = 'TENTE UM NUMERO MAIOR';
+            
         }
+
+        hintField.focus();
     }
-    
-    //regHints(userInput);
+    hintField.value = '';
+    regHints(userInput);
     cont++;
 }
+
+regHints = function(userInput){
+    let blockHint = document.createElement('p');
+    blockHint.textContent = userInput;
+    
+    hints.appendChild(blockHint);
+} 
+
+//function gameOver(){}
