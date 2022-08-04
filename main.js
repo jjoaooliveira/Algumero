6fb3505315165db521b9b3a5b41fcf0750234159
Joao Oliveira <88111035+jrOliv@users.noreply.github.com>
2022-08-03 20:16:17 -0400

    main.js 1.1.4

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 let num = Math.floor((Math.random()*100)+1);
-
 const hintField = document.querySelector('.hintField');
 const hints = document.querySelector('.hints'); 
-const gameStatus = document.querySelectorAll('.gameStatus > p');
+
 console.log(num);
 
 //hintButton.addEventListener('click', checkNumber);
@@ -21,25 +20,23 @@ function checkNumber(){
     console.log(userInput);
     
     if(userInput === num){
-        gameStatus[0].textContent = '-ACERTOU-';
-        gameStatus[1].textContent = '';
+        
 
         gameOver();
 
     } else if(cont === 9){
-        gameStatus[0].textContent = '-FIM DE JOGO-';
-        gameStatus[1].textContent = '';
+    
 
         gameOver();
 
     } else {
-        gameStatus[0].textContent = '-ERRADO-';
+       
 
         if(userInput > num){
-            gameStatus[1].textContent = 'TENTE UM NUMERO MENOR';
+            
 
         } else {
-            gameStatus[1].textContent = 'TENTE UM NUMERO MAIOR';
+            
 
         }
 
@@ -53,6 +50,7 @@ function checkNumber(){
 
 regHints = function(userInput){
     let blockHint = document.createElement('p');
+    blockHint.setAttribute('class', 'box');
     blockHint.textContent = userInput;
     
     hints.appendChild(blockHint);
@@ -60,21 +58,16 @@ regHints = function(userInput){
 
 function gameOver(){
     hintField.disabled = true;
-    hintButton.disabled = true;
-    
+
 }
 
 restartGame = function(){
-    num = Math.floor(Math.random()*100)+1;
     let boxHints = document.querySelectorAll('.hints > p');
+    
+    num = Math.floor(Math.random()*100)+1;
     cont = 0;
     
     hintField.disabled = false;
-    hintButton.disabled = false;
-    
-    
-    gameStatus[0].textContent = '';
-    gameStatus[1].textContent = '';
 
     for(let i = 0; i <= boxHints.length - 1; i++){
         hints.removeChild(boxHints[i]); 
