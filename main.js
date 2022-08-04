38e12da2a8e9b825c7492125911ed4f9c4a2af98
Joao Oliveira <88111035+jrOliv@users.noreply.github.com>
2022-08-03 20:15:35 -0400

    main.js 1.1.4
    button removido e div gamestatus removido.

--- main.js
+++ main.js
@@ -1,120 +1,75 @@
-//gera o numero
-let numeroAleatorio = Math.floor(Math.random()*100)+1;
-console.log(numeroAleatorio);
+let num = Math.floor((Math.random()*100)+1);
+const hintField = document.querySelector('.hintField');
+const hints = document.querySelector('.hints'); 
 
-let userInput = document.querySelector('.campoPalpite');
-let trigger = document.querySelector('.envioPalpite');
-let result = document.querySelector('.resultado')
-let dicas = document.querySelector('.dica');
-//let historico = document.querySelector('.palpites');
+console.log(num);
 
-let botaoReinicio;
-let contagem = 1;
+//hintButton.addEventListener('click', checkNumber);
 
-userInput.focus();
+document.addEventListener('keydown', function(e){
+    if(e.key === 'Enter'){
+        checkNumber();
+    }
+});
+
+let cont = 0;
+hintField.focus();
 
 function checkNumber(){
-    let palpite = Number(userInput.value);
-    //console.log(palpite);
-
-    /*if (contagem === 1) {
-        historico.textContent = 'Palpites: ';
-    }*/
-
-    if(palpite === numeroAleatorio){
-        result.textContent = '-ACERTOU-';
-        dicas.textContent = '';
-        //historico.textContent += palpite;
-        //console.log('acerto: ' + palpite);
-        fimDeJogo();
+    let userInput = Number(hintField.value);
+    console.log(userInput);
+    
+    if(userInput === num){
         
-    } else if(contagem === 10){
-        result.textContent = '-FIM DE JOGO-';
-        dicas.textContent = '';
-        //historico.textContent += palpite;
-        //console.log('fim: ' + palpite);
-        fimDeJogo();
+
+        gameOver();
+
+    } else if(cont === 9){
+    
+
+        gameOver();
 
     } else {
-        result.textContent = '-ERRADO-'
-        //historico.textContent += palpite + ' ';
-        historicoBlocks(palpite);
+       
 
-        if (palpite > numeroAleatorio) {
-            dicas.textContent = 'Palpite maior que o numero';
-            //console.log('erro>: ' + palpite);
+        if(userInput > num){
+            
 
         } else {
-            dicas.textContent = 'Palpite é menor que o numero';
-            //console.log('erro<: ' + palpite);
+            
+
         }
+
     }
-    contagem++;
-    userInput.value = '';
-    userInput.focus();
-
+    hintField.focus();
+    hintField.value = '';
+    
+    regHints(userInput);
+    cont++;
 }
-trigger.addEventListener('click', checkNumber);
 
-function fimDeJogo(){
-    userInput.disabled = true;
-    trigger.disabled = true;
-    botaoReinicio = document.createElement('button');
-    botaoReinicio.textContent = 'Reiniciar o jogo';
-    document.body.appendChild(botaoReinicio);
-    botaoReinicio.addEventListener('click', gameReinicio);
+regHints = function(userInput){
+    let blockHint = document.createElement('p');
+    blockHint.setAttribute('class', 'box');
+    blockHint.textContent = userInput;
+    
+    hints.appendChild(blockHint);
+} 
 
-    botaoReinicio.style.width = '150px';
-    botaoReinicio.style.height = '40px';
-    botaoReinicio.style.borderRadius = '10px';
-    botaoReinicio.style.border = '1px solid black'; 
+function gameOver(){
+    hintField.disabled = true;
 
 }
 
-function gameReinicio(){
-    contagem = 1;
-    let restartGameStatus = document.querySelectorAll('.gameStatus p');
+restartGame = function(){
+    let boxHints = document.querySelectorAll('.hints > p');
+    
+    num = Math.floor(Math.random()*100)+1;
+    cont = 0;
+    
+    hintField.disabled = false;
 
-    for (let i = 0; i < restartGameStatus.length; i++) {
-        restartGameStatus[i].textContent = '';
+    for(let i = 0; i <= boxHints.length - 1; i++){
+        hints.removeChild(boxHints[i]); 
     }
-
-    botaoReinicio.parentNode.removeChild(botaoReinicio);
-
-    trigger.disabled = false;
-    userInput.disabled = false;
-    userInput.value = '';
-    numeroAleatorio = Math.floor(Math.random()*100)+1; 
 }
-
-function historicoBlocks(palpite){
-    let core = document.querySelector('.palpites');
-    let bloco = document.createElement('p');
-
-    bloco.textContent = palpite;
-    core.appendChild(bloco);
-}
-
-
-/*var list = document.querySelector('.output ul');
-list.innerHTML = '';
-var stations = ['MAN675847583748sjt567654;Manchester Piccadilly',
-                'GNF576746573fhdg4737dh4;Greenfield',
-                'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
-                'SYB4f65hf75f736463;Stalybridge',
-                'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'];
-
-for (var i = 0; i < stations.length; i++) {
-  var input = stations[i];
-  
-  let code3 = input.slice(0,3);
-  let codeNameStation = input.slice(input.indexOf(';') +1, input.length);
-  let nameStation = code3 + ': ' + codeNameStation; 
-
-  var result = nameStation;
-  var listItem = document.createElement('li');
-  listItem.textContent = result;
-  list.appendChild(listItem);
-}
-*/
-
